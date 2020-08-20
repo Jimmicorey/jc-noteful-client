de026b4e61456fcb3d7f0442a3b57fb5d28da0f9
James Corey Coffelt <james.c.coffelt@gmail.com>
2020-08-20 14:40:51 -0500

    api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: `https://stormy-harbor-24498.herokuapp.com/api/`,
+    API_ENDPOINT: `https://stormy-harbor-24498.herokuapp.com/api`,
     API_KEY: process.env.REACT_APP_API_KEY
 }
